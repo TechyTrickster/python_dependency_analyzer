a1545cab428deee2e8bc942fb849653a260864fb
Andrew <mylifescrazy.phifer19@gmail.com>
2024-10-30 00:02:30 -0500

    added the following features: 1. nicer rendering of from type imports 2. main now returns a dict instead of a jumbled up list 3. output shows a list of all custom source files in the project 4. output will show used and unused source files
    TODO:
    1. add documentation to existing functions
    2. augment current systems with recursive functionality to build more representitive outputs

--- source/analyze.py
+++ source/analyze.py
@@ -15,10 +15,37 @@ sys.path.append(str(rootDir))
 sys.path = sorted(list(set(sys.path)))
 initial = str(Process.pid)
 
+from source import example
 
+
+#TODO: Write DocString
 def generateSourceTokens(inputSource: str) -> list[str]:
     output = []
 
+    return output
+
+
+def generateProjectTree(rootPath: Path) -> list[str]:    
+    genLocal = lambda node, leaves : reduce(lambda y, x : y + [node / x], leaves, [])
+    walkResults = os.walk(rootPath)
+    output = reduce(lambda y, x : y + genLocal(Path(x[0]), x[2]), walkResults, [])
+    return output
+
+
+def generateImportAliases(projectRoot: Path, paths: list[Path], venvPath: Path) -> list[str]:    
+    projectRootStr = str(projectRoot)
+    venvPathStr = str(venvPath)
+    createImportAlias = lambda x : str(x).replace(projectRootStr, "").replace("/", ".")
+    filterLeadingDot = lambda x : x[1:] if x[0] == "." else x    
+    containsVenvPath = lambda x : not startsWith2(venvPathStr, str(x))
+    isAPyFile = lambda x : x.suffix == ".py"
+    removePythonExtension = lambda x : re.sub(r'\.py$', '', x)
+    buffer0 = list(filter(isAPyFile, paths))
+    buffer1 = list(filter(containsVenvPath, buffer0))
+    buffer2 = list(map(createImportAlias, buffer1))
+    buffer3 = list(map(filterLeadingDot, buffer2))    
+    buffer4 = list(map(removePythonExtension, buffer3))
+    output = list(zip(buffer1, buffer4))    
     return output
 
 
@@ -45,6 +72,30 @@ def startsWith(pattern: str, body: str) -> bool:
     return output
 
 
+def startsWith2(pattern: str, body: str) -> bool:
+    """
+    determines if the string in body starts with the string in pattern, as a non-isolated token
+
+    Parameters
+    ----------
+    pattern : str
+        the string to search for in the body parameter.  only use string literals
+    body : str
+        the body of text to search using the given pattern
+
+    Returns
+    -------
+    bool
+        indication of whether the pattern string was found at the beginning of the body    
+    """
+
+    regex = f'^{pattern}'
+    hits = re.findall(regex, body)
+    output = (len(hits) > 0)
+    return output
+
+
+#TODO: Write DocString
 def endsWith(pattern: str, body: str) -> bool:
     regex = f' {pattern}^'
     hits = re.findall(regex, body)
@@ -52,6 +103,7 @@ def endsWith(pattern: str, body: str) -> bool:
     return output
 
 
+#TODO: Write DocString
 def advancedContains(pattern: str, body: str) -> bool:
     start = startsWith(pattern, body)
     mid = (" " + pattern + " ") in body
@@ -60,6 +112,7 @@ def advancedContains(pattern: str, body: str) -> bool:
     return output
 
 
+#TODO: Write DocString
 def isLineImport(input: str) -> bool:
     keywords = list(keyword.kwlist)
     keywords.remove("import")
@@ -68,7 +121,8 @@ def isLineImport(input: str) -> bool:
     output = startsWith("import", input) and not containsForbiddens
     return output
 
-    
+
+#TODO: Write DocString
 def isLineFrom(input: str) -> bool:
     keywords = list(keyword.kwlist)
     keywords.remove("import")
@@ -79,28 +133,28 @@ def isLineFrom(input: str) -> bool:
     return output
 
 
-def processImportLine(inputLine: str) -> list[str]:
-    print(inputLine)
-    buffer0 = re.sub(r'^import ', '', inputLine)
-    print(buffer0)
+#TODO: Write DocString
+def processImportLine(inputLine: str) -> list[str]:    
+    buffer0 = re.sub(r'^import ', '', inputLine)    
     buffer1 = buffer0.split(",")
     output = list(map(lambda x : x.strip(), buffer1))
     return output
 
 
+#TODO: Write DocString
 def processFromLine(inputLine: str) -> tuple[str, list[str]]:    
     processedLine = re.sub(r'\s+', ' ', inputLine)
     buffer = processedLine.split(" ")
     rootImport = buffer[1]
     leafImportString = processedLine.replace(rootImport, "").replace("import", "").replace("from", "").strip()
     leafImports = list(map(lambda x : x.strip(), leafImportString.split(",")))
-    output = (rootImport, leafImports)
+    output = list(map(lambda x : rootImport + "." + x, leafImports))    
     return output
 
 
 
-def main(initialScript: Path):
-    output = []
+#TODO: Write DocString
+def main(initialScript: Path, projectRootDirectory: Path, venvDirectory: Path) -> dict:    
     fileHandle = open(initialScript, "r")
     data = fileHandle.read()
     fileHandle.close()
@@ -108,13 +162,39 @@ def main(initialScript: Path):
     importDependencies = list(filter(isLineImport, lines))
     fromDependencies = list(filter(isLineFrom, lines))
     processedImports = list(reduce(lambda y, x : y + processImportLine(x), importDependencies, []))
-    processedFroms = list(reduce(lambda y, x : y + [processFromLine(x)], fromDependencies, []))
-    output.extend(processedImports)
-    output.extend(processedFroms)
+    processedFroms = list(reduce(lambda y, x : y + processFromLine(x), fromDependencies, []))
+    
+    allImports = []
+    allImports.extend(processedImports)
+    allImports.extend(processedFroms)    
+    paths = generateProjectTree(projectRootDirectory)
+    customAssets = generateImportAliases(projectRootDirectory, paths, venvDirectory)
+    customAssetsShortNames = list(map(lambda x : x[1], customAssets))
+    rootFileShortName = list(filter(lambda x : x[0] == initialScript, customAssets))[0][1]
+    allImports.append(rootFileShortName)
+    usedSources = list(filter(lambda x : x[1] in allImports, customAssets))
+    
+    standardImportsBuffer = []
+    standardImportsBuffer.extend(processedImports)
+    standardImportsBuffer.extend(processedFroms)    
+    standardImports = list(filter(lambda x : x not in customAssetsShortNames, standardImportsBuffer))
+
+    unusedSourceFiles = list(filter(lambda x : x not in usedSources, customAssets))
+
+    output = {
+        'standard imports': standardImports,
+        'all source files': customAssets,
+        'used source files': usedSources,
+        'unused source files': unusedSourceFiles
+    }
+    
     return output
 
 
+#TODO: Write DocString
 if __name__ == "__main__":
-    startingScript = sys.argv[1]
-    buffer = main(startingScript)
+    startingScript = Path(sys.argv[1]).resolve()
+    projectRootDirectory = Path(sys.argv[2]).resolve()
+    venvDirectory = Path(sys.argv[3]).resolve()
+    buffer = main(startingScript, projectRootDirectory, venvDirectory)
     print(buffer)
